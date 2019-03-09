261e7a459c52fec5b6e98374752599d7215cbc60
Will Olson <will.olson11@gmail.com>
2019-03-09 14:19:56 -0800

    Allow analytics to be accesses from the browser too

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resource :short_link, only: :create
 
-  get '/:short_code+', to: 'short_links/analytics#show', as: :short_link_analytics
-  get '/:short_code',  to: 'short_links#show',           as: :shortened
+  get '/:short_code+', to: 'short_links/analytics#show',
+    as: :short_link_analytics, defaults: { format: 'json' }
+
+  get '/:short_code', to: 'short_links#show', as: :shortened
 end
--- test/controllers/short_links/analytics_controller_test.rb
+++ test/controllers/short_links/analytics_controller_test.rb
@@ -3,8 +3,16 @@ require 'test_helper'
 module ShortLinks
   class AnalyticsControllerTest < ActionDispatch::IntegrationTest
     test 'should redirect short_link' do
+      # API access
       short_link = short_links(:kapost)
       get short_link_analytics_url(short_link.short_code), as: :json
+
+      assert_response :ok
+      assert_equal short_link.visits.size, response.parsed_body.size
+
+      # HTML access
+      short_link = short_links(:kapost)
+      get short_link_analytics_url(short_link.short_code)
 
       assert_response :ok
       assert_equal short_link.visits.size, response.parsed_body.size
